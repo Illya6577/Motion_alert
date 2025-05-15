22170e598d6271ba9964c6502e1bce353cbb44a1
Ілля <108460564+Illya6577@users.noreply.github.com>
2025-05-15 05:56:38 +0000

    Remove debug light

--- Motion.ino
+++ Motion.ino
@@ -1,5 +1,4 @@
 int pirPin = 7;
-int ledPin = 13;
 
 int val = 0;
 
@@ -7,23 +6,17 @@ void setup()
 {
 Serial.begin(9600);
 pinMode(pirPin, INPUT);
-pinMode(ledPin, OUTPUT);
-pinMode(3, OUTPUT);
 }
 
 void loop () {
 val = digitalRead(pirPin);
 if (val == HIGH)
     {
-    	 digitalWrite(ledPin, LOW);
     	 Serial.println("Motion!");
-  		 digitalWrite(3,HIGH);
     }
 else
     {
-      	digitalWrite(ledPin, LOW);
       	Serial.println("No motion");
-  		digitalWrite(3,LOW);
     }
 delay(100);
 }
